7ebb1cf1f5d6d4c12cb0758ebee6f86157242b19
EmelyanovArtem <92636352+EmelyanovArtem@users.noreply.github.com>
2022-06-16 16:14:05 +0300

    Добавление файлов

---
+++ js/myscript.js
@@ -0,0 +1,21 @@
+function consoleBG() {
+    var url = "img/bg-whitecream.jpg";
+    if ($(window).scrollTop() < 5) {
+      $('.header__row--top').css({'background-image': 'url("' + url + '")',
+                            'background-position': 'right', 
+                            'background-size': '45% 100%', 
+                            'background-repeat': 'no-repeat',
+                            'background-color' : 'transparent',
+                            'box-shadow': 'none'});
+        console.log(2);
+    } else {
+      $('.header__row--top').css({'background': 'white',
+                             'box-shadow': '0px 4px 15px rgba(0, 0, 0, 0.05)'});
+      console.log(1);
+    }
+  }
+  consoleBG();
+
+  $(window).scroll(function() {
+    consoleBG();
+  });
